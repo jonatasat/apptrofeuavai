a31402dbe39770fb1768726cd9229fb640e27996
jonatasat <jonatas.redes@gmail.com>
2017-11-18 15:54:54 -0200

    Adicionado formulários de adicionar e deletar jogadores

--- app/src/app/app.routing.ts
+++ app/src/app/app.routing.ts
@@ -1,12 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PlayersComponent } from './players/players.component';
+import { AddPlayerComponent } from './players/add-player/add-player.component';
+import { EditPlayerComponent } from './players/edit-player/edit-player.component';
 import { HomeComponent } from './home/home.component';
 import { ModuleWithProviders } from '@angular/core';
 
 const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'players', component: PlayersComponent }
+    { path: 'players', component: PlayersComponent },
+    { path: 'newplayer', component: AddPlayerComponent},
+    { path: 'editplayer', component: EditPlayerComponent}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
