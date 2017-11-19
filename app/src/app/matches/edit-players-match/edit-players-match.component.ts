64fcba66c2ea5e779351aa111143c2196ebe2f04
jonatasat <jonatas.redes@gmail.com>
2017-11-19 13:36:36 -0200

    Adicionado gerenciamento de partidas

---
+++ app/src/app/matches/edit-players-match/edit-players-match.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-players-match',
+  templateUrl: './edit-players-match.component.html',
+  styleUrls: ['./edit-players-match.component.css']
+})
+export class EditPlayersMatchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
