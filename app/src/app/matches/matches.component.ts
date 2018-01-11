ad44f28d150f230f50f7c5e4b1f93fcc9a717321
jonatasat <jonatas.redes@gmail.com>
2018-01-11 20:11:38 -0200

    add pagination in others pages

--- app/src/app/matches/matches.component.ts
+++ app/src/app/matches/matches.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PaginationService } from '../pagination.service'
+
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-matches',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatchesComponent implements OnInit {
 
-  constructor() { }
+  private allItems: any[] = [];
+
+  pager: any = {};
+
+  pagedItems: any[];
+
+  constructor(private paginationService: PaginationService) { }
+
 
   ngOnInit() {
+    
+    for(let i=0; i< 10; i++){
+      this.allItems.push(
+        {
+          row: i,
+          time: 'Time' + i,
+          placar: i + " x " + i,
+          adversario: 'Adversario' + i,
+          estadio: 'Estadio' + i,
+          media: i + ',' + i
+        }
+      );
+    }
+
+    this.setPage(1);
+  }
+
+  setPage(page: number) {
+    if (page < 1 || page > this.pager.totalPages) {
+      return;
+    }
+
+    this.pager = this.paginationService.getPager(this.allItems.length, page);
+
+    this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
   }
 
 }
--- app/src/app/players/players.component.ts
+++ app/src/app/players/players.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PaginationService } from '../pagination.service'
+
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-players',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayersComponent implements OnInit {
 
-  constructor() { }
+  private allItems: any[] = [];
+
+  pager: any = {};
+
+  pagedItems: any[];
+
+  constructor(private paginationService: PaginationService) { }
+
 
   ngOnInit() {
+    
+    for(let i=0; i< 10; i++){
+      this.allItems.push(
+        {
+          row: i,
+          name: 'Jogador' + i,
+          posicao: 'Posicao' + i
+        }
+      );
+    }
+
+    this.setPage(1);
+  }
+
+  setPage(page: number) {
+    if (page < 1 || page > this.pager.totalPages) {
+      return;
+    }
+
+    this.pager = this.paginationService.getPager(this.allItems.length, page);
+
+    this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
   }
 
 }
