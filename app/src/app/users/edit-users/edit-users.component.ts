249bea1f1929fa9775fec06913052bb9904dbcce
jonatasat <jonatas.redes@gmail.com>
2018-01-27 16:48:28 -0200

    edit firebase user

--- app/src/app/users/edit-users/edit-users.component.ts
+++ app/src/app/users/edit-users/edit-users.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Http } from '@angular/http';
-import 'rxjs/add/operator/map'
 import { ActivatedRoute } from '@angular/router';
+import {AngularFireDatabase} from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireObject } from 'angularfire2/database/interfaces';
 
 @Component({
   selector: 'app-edit-users',
@@ -11,17 +13,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EditUsersComponent implements OnInit {
 
-  id: string;
+  user;
 
-  constructor(private route: ActivatedRoute) { 
+  constructor(private route: ActivatedRoute, private db: AngularFireDatabase) { 
     console.log(this.route.snapshot.params['id']);
   }
 
-  user: any = {
-    name: 'Jonatas'
-  }
-
   ngOnInit() {
+    this.user = this.db.object('users/'+ this.route.snapshot.params['id']).valueChanges();
+    console.log(this.user);
+
+    // this.users = this.db.list('users' + this.route.snapshot.params['id']).snapshotChanges().map(actions =>{
+    //   return actions.map(action => ({ key: action.key, ...action.payload.val() }));
+    // });
+    // this.user = this.db.list('users' + this.route.snapshot.params['id']);
+    // this.users = this.db.list('users').snapshotChanges().map(items =>{
+    //   return items.map(item => {
+    //       item.key = this.route.snapshot.params['id'];
+    //       return item;
+    //   }); 
+    // });
   }
 
   onSubmit(form){
