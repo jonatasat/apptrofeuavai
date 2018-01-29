9318f2029d79ba2779046cda5008e82cb5e4eaeb
jonatasat <jonatas.redes@gmail.com>
2018-01-28 22:40:41 -0200

    users crud firebase final

--- app/src/app/users/edit-users/edit-users.component.ts
+++ app/src/app/users/edit-users/edit-users.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Http } from '@angular/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import {AngularFireDatabase} from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireObject } from 'angularfire2/database/interfaces';
@@ -15,30 +15,27 @@ export class EditUsersComponent implements OnInit {
 
   user;
 
-  constructor(private route: ActivatedRoute, private db: AngularFireDatabase) { 
+  constructor(private route: ActivatedRoute, private db: AngularFireDatabase, private router: Router) { 
     console.log(this.route.snapshot.params['id']);
   }
 
   ngOnInit() {
     this.user = this.db.object('users/'+ this.route.snapshot.params['id']).valueChanges();
-    console.log(this.user);
-
-    // this.users = this.db.list('users' + this.route.snapshot.params['id']).snapshotChanges().map(actions =>{
-    //   return actions.map(action => ({ key: action.key, ...action.payload.val() }));
-    // });
-    // this.user = this.db.list('users' + this.route.snapshot.params['id']);
-    // this.users = this.db.list('users').snapshotChanges().map(items =>{
-    //   return items.map(item => {
-    //       item.key = this.route.snapshot.params['id'];
-    //       return item;
-    //   }); 
-    // });
   }
 
   onSubmit(form){
     // this.http.post('https://httpbin.org/post', JSON.stringify(form.value))
     // .map(res => res)
     // .subscribe(dados => console.log);
+
+    this.db.database.ref('users/'+ this.route.snapshot.params['id']).set({
+        name: form.value.name,
+        login: form.value.login,
+        password: form.value.password,
+        confirmpassword: form.value.confirmpassword
+    });
+
+      this.router.navigate(['/users']);
   }
 
 }
