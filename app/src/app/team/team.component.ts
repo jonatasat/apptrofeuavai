e9d383bf652d3e26cd45e621179d6fb60f56f7f6
jonatasat <jonatas.redes@gmail.com>
2018-01-30 20:01:24 -0200

    Firebase team crud

--- app/src/app/team/team.component.ts
+++ app/src/app/team/team.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { PaginationService } from '../pagination.service'
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 import * as _ from 'underscore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-team',
@@ -11,20 +14,18 @@ import * as _ from 'underscore';
 })
 export class TeamComponent implements OnInit {
 
-  private allItems: any[] = [];
-
+  allItems: any[] = [];
   pager: any = {};
-
   pagedItems: any[];
-
-  teams: FirebaseListObservable<any[]>;
+  teams: Observable<any[]>;
 
   constructor(private paginationService: PaginationService, private db: AngularFireDatabase) { }
 
   ngOnInit() {
-    this.teams = this.db.list('teams');
+    this.teams = this.db.list('teams').snapshotChanges().map(actions =>{
+      return actions.map(action => ({ key: action.key, ...action.payload.val() }));
+    });
     this.allItems.push(this.teams);
-    
     this.setPage(1);
   }
 
@@ -32,10 +33,13 @@ export class TeamComponent implements OnInit {
     if (page < 1 || page > this.pager.totalPages) {
       return;
     }
-
     this.pager = this.paginationService.getPager(this.allItems.length, page);
-
     this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
+  }
+
+  deleteItem(item){
+    const itemsRef = this.db.list('users');
+    itemsRef.remove(item.key);
   }
 
 
