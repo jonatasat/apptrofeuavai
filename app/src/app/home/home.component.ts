6c65a47bb9860caca06ef8a5e2c3381543a48663
jonatasat <jonatas.redes@gmail.com>
2018-02-22 22:14:44 -0300

    field initial settings

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { ActivatedRoute, Router } from '@angular/router';
+import {AngularFireDatabase} from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireObject } from 'angularfire2/database/interfaces';
+import { PaginationService } from '../pagination.service';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { FirebaseApp } from 'angularfire2';
+import 'firebase/storage';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +18,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  storageRef: any;
+  matches: any;
+
+  constructor(private route: ActivatedRoute, private db: AngularFireDatabase, private router: Router, private paginationService: PaginationService, private firebase: FirebaseApp) { }
+
+  players: any;
 
   ngOnInit() {
+    this.storageRef = this.firebase.storage().ref();
+    this.matches = this.db.list('matches').snapshotChanges().map(actions =>{
+      return actions.map(action => ({ key: action.key, ...action.payload.val() }));
+    });
+  }
+
+  onSubmit(form){
+    let id = form.value.match.key;
+    console.log(form.value.match.key);
+
+    this.storageRef = this.firebase.storage().ref();
+    this.players = this.db.list('matches/'+id+'/players/').snapshotChanges().map(actions =>{
+      return actions.map(action => ({ key: action.key, ...action.payload.val() }));
+    });
   }
 
 }
