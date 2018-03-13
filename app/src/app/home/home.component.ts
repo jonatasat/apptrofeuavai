fc8006b2c588c61b3f58316ce22f33e21999fc28
jonatasat <jonatas.redes@gmail.com>
2018-03-12 21:56:41 -0300

    complete avg grade players and substitutes

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { ActivatedRoute, Router } from '@angular/router';
-import {AngularFireDatabase} from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireObject } from 'angularfire2/database/interfaces';
 import { PaginationService } from '../pagination.service';
@@ -18,7 +18,7 @@ import 'firebase/storage';
 })
 export class HomeComponent implements OnInit {
 
- 
+
 
   constructor(private route: ActivatedRoute, private db: AngularFireDatabase, private router: Router, private paginationService: PaginationService, private firebase: FirebaseApp) { }
 
@@ -36,41 +36,59 @@ export class HomeComponent implements OnInit {
   matches: any;
   match: any;
   matchesList: any[] = [];
-  showAverage=false;
+  listGrades: any[] = [];
+  listGradesSubstitutes: any[] = [];
+  showAverage = false;
   matchesPlayers: any[] = [];
-  sumGrades: any = 0;
-  
+
 
   ngOnInit() {
     this.storageRef = this.firebase.storage().ref();
-    this.matches = this.db.list('matches').snapshotChanges().map(actions =>{
+    this.matches = this.db.list('matches').snapshotChanges().map(actions => {
       return actions.map(action => ({ key: action.key, ...action.payload.val() }));
     });
   }
 
-  onSubmit(form){
+  onSubmit(form) {
     let id = form.value.match.key;
 
     this.storageRef = this.firebase.storage().ref();
 
-    this.players = this.db.list('matches/'+id+'/players/').snapshotChanges().map(actions =>{
+    this.players = this.db.list('matches/' + id + '/players/').snapshotChanges().map(actions => {
       return actions.map(action => ({ key: action.key, ...action.payload.val() }));
     });
 
-    this.substitutes = this.db.list('matches/'+id+'/substitutes/').snapshotChanges().map(actions =>{
+    this.substitutes = this.db.list('matches/' + id + '/substitutes/').snapshotChanges().map(actions => {
       return actions.map(action => ({ key: action.key, ...action.payload.val() }));
     });
 
     this.firebase.database().ref("matches/" + id).once('value', data => this.getMatch(data), this.errData);
 
-    this.firebase.database().ref("matches/" + id +'/players/').once('value', data => this.getPlayers(data), this.errData);
+    this.firebase.database().ref("matches/" + id + '/players/').once('value', data => this.getPlayers(data), this.errData);
 
-    
-    this.showAverage=true;
+    this.firebase.database().ref("matches/" + id + '/substitutes/').once('value', data => this.getSubstitutes(data), this.errData);
+
+
+    let sum = 0;
+    this.listGrades.map(function (value, index) {
+      sum = sum + parseFloat(value);
+    });
+
+    let sumSubstitutes = 0;
+    this.listGradesSubstitutes.map(function (value, index) {
+      sumSubstitutes = sumSubstitutes + parseFloat(value);
+    });
+
+    let soma = sum + sumSubstitutes;
+    let somaTamanhos = this.listGrades.length + this.listGradesSubstitutes.length;
+    let total = soma / somaTamanhos;
+
+    this.average = total.toFixed(2);
+
+    this.showAverage = true;
   }
 
-  getMatch(data){
-    this.average = data.val().average;
+  getMatch(data) {
     this.opponent = data.val().opponent;
     this.team = data.val().team;
     this.score = data.val().score;
@@ -79,11 +97,19 @@ export class HomeComponent implements OnInit {
     this.date = data.val().date.split('-')[2] + '/' + data.val().date.split('-')[1] + '/' + data.val().date.split('-')[0];
   }
 
-  getPlayers(data){
-    Object.values(data.val()).forEach(this.logelements);
+  getPlayers(data) {
+    this.listGrades = Object.keys(data.val()).map(function (key) {
+      return data.val()[key].grade;
+    });
   }
 
-  logelements(element){
+  getSubstitutes(data){
+    this.listGradesSubstitutes = Object.keys(data.val()).map(function (key) {
+      return data.val()[key].grade;
+    });
+  }
+
+  logelements(element) {
     return Number(element.grade);
   }
 
